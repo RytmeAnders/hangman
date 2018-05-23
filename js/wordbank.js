1e51d3c664234d69722a216881e6abb013936270
puna47 <puna47@gmail.com>
2018-05-23 13:25:25 +0200

    Design
    Design, more words

--- js/wordbank.js
+++ js/wordbank.js
@@ -4,5 +4,14 @@ Words.List = [];
 // Don't use UppercaseLetters in the word it will break
 
 Words.List[0] = "carol";
+Words.List[1] = "anders";
+Words.List[2] = "gintare";
+Words.List[3] = "teamwork";
+Words.List[4] = "summer";
+Words.List[5] = "hangman";
+Words.List[6] = "computer";
+Words.List[7] = "mouse";
+Words.List[8] = "banana";
+Words.List[9] = "water";
 
 Words.Length = Words.List.length;
